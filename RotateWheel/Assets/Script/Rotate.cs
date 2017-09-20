9377ec0c1e12b8e691fa7d85f19b24ba8982339d
long.duongxuan <long.duongxuan@gameloft.com>
2017-09-20 16:10:47 +0700

    * Add first version

---
+++ RotateWheel/Assets/Script/Rotate.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+	#region Motor
+
+	public int m_Speed = 50;
+	public float m_MaxSpeed = 5000;
+	public float m_CelerationRate = 500;
+	public float m_BrakeSpeed = 2500;
+
+	float m_Slope = 0;
+	float m_TorqueDir = 0;
+	float m_Dir = 0;
+	Rigidbody2D m_Body;
+	WheelJoint2D m_Wheel;
+	JointMotor2D m_Motor;
+
+	#endregion
+
+	// Use this for initialization
+	void Start ()
+	{
+		m_Body = GetComponent<Rigidbody2D> ();
+		m_Wheel = GetComponent<WheelJoint2D> ();
+		m_Motor = m_Wheel.motor;
+	}
+
+	void FixedUpdate ()
+	{
+		m_TorqueDir = Input.GetAxis ("Horizontal");
+		if (m_TorqueDir != 0)
+			m_Body.AddTorque (3 * Mathf.PI * m_TorqueDir, ForceMode2D.Force);
+		else
+			m_Body.AddTorque (0);
+
+		m_Slope = transform.localEulerAngles.z;
+
+		if (m_Slope >= 180)
+			m_Slope -= 360;
+
+		m_Dir = Input.GetAxis ("Horizontal");
+		if (m_Dir != 0) {
+			m_Motor.motorSpeed = Mathf.Clamp (m_Motor.motorSpeed - (m_Dir * m_CelerationRate - Mathf.Sin ((m_Slope * Mathf.PI) / 180) * 80) * Time.deltaTime, -m_MaxSpeed, m_MaxSpeed);
+		}
+
+		if ((m_Dir == 0 && m_Motor.motorSpeed < 0) || (m_Dir==0 && m_Motor.motorSpeed == 0 && m_Slope < 0)){
+			m_Motor.motorSpeed = Mathf.Clamp(m_Motor.motorSpeed - ((-m_CelerationRate) - Mathf.Sin((m_Slope * Mathf.PI) / 180)* 80)*Time.deltaTime, -m_MaxSpeed, 0);
+		}
+		else if ((m_Dir == 0 && m_Motor.motorSpeed > 0) || (m_Dir == 0 && m_Motor.motorSpeed == 0 && m_Slope > 0)){
+			m_Motor.motorSpeed = Mathf.Clamp(m_Motor.motorSpeed - (m_CelerationRate - Mathf.Sin((m_Slope * Mathf.PI)/180)*80)*Time.deltaTime, 0, m_MaxSpeed);
+		}
+
+		m_Wheel.motor = m_Motor;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+//		if (Input.GetKey(KeyCode.RightArrow))
+//		{
+//			m_Motor = m_Wheel.motor;
+//			m_Motor.motorSpeed += m_Speed;
+//			if (m_Motor.motorSpeed >= m_Motor.maxMotorTorque)
+//				m_Motor.motorSpeed = m_Motor.maxMotorTorque;
+//			m_Wheel.motor = m_Motor;
+//		}
+//		
+//		if (Input.GetKey(KeyCode.LeftArrow))
+//		{
+//			m_Motor = m_Wheel.motor;
+//			m_Motor.motorSpeed -= m_Speed;
+//			if (Mathf.Abs (m_Motor.motorSpeed) >= m_Motor.maxMotorTorque)
+//				m_Motor.motorSpeed = 0 - m_Motor.maxMotorTorque;
+//			m_Wheel.motor = m_Motor;
+//		}	
+	}
+}
---
+++ RotateWheel/Assets/Script/SampleScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScript : MonoBehaviour {
+	public Vector3 m_Position;
+
+	public GameObject m_Left;
+	public GameObject m_Right;
+	// Use this for initialization
+
+	private Vector3 m_CurrentEulerAngle;
+	private Quaternion m_CurrentQua;
+	//private Rigidbody2D m_Body;
+	void Start () {
+		//m_Body = GetComponent<Rigidbody2D> ();
+		m_Position = gameObject.transform.position;
+		m_CurrentEulerAngle = gameObject.transform.eulerAngles;
+		m_CurrentQua = Quaternion.LookRotation (gameObject.transform.eulerAngles);
+	}
+
+	void FixedUpdate()
+	{
+		//Vector3 origin = gameObject.transform.position;
+		Vector3 origin = m_Position;
+		Vector3 angle = gameObject.transform.eulerAngles - m_CurrentEulerAngle;
+		Quaternion next = Quaternion.LookRotation (gameObject.transform.eulerAngles);
+
+		if (m_Left != null && angle != Vector3.zero) {
+			RotateObject (ref m_Left, origin, angle);
+			//RotateObject(ref m_Left, origin, next);
+		}
+
+		if (m_Right != null && angle != Vector3.zero) {
+			RotateObject (ref m_Right, origin, angle);
+			//RotateObject(ref m_Right, origin, next);
+		}
+
+		m_CurrentEulerAngle = gameObject.transform.eulerAngles;
+		m_CurrentQua = next;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void RotateObject (ref GameObject obj, Vector3 position, Quaternion next)
+	{
+		Quaternion delta = next;// - m_CurrentQua;
+		Vector3 objpos = obj.transform.position;
+		float x = position.x + (objpos.x - position.x) * Mathf.Cos (delta.eulerAngles.z * Mathf.PI / 180) - (objpos.y - position.y) * Mathf.Sin (delta.eulerAngles.z * Mathf.PI / 180);
+		float y = position.y + (objpos.x - position.x) * Mathf.Sin (delta.eulerAngles.z * Mathf.PI / 180) + (objpos.y - position.y) * Mathf.Cos (delta.eulerAngles.z * Mathf.PI / 180);
+
+		//obj.transform.position = new Vector3 (x, y, objpos.z);
+		obj.transform.position = Vector3.Lerp(obj.transform.position, new Vector3(x, y, objpos.z), Time.fixedDeltaTime);
+		//obj.transform.Translate(new Vector3(x, y, objpos.z));
+		obj.transform.rotation = Quaternion.Slerp(m_CurrentQua, next, Time.fixedDeltaTime);
+	}
+
+	private void RotateObject (ref GameObject obj, Vector3 position, Vector3 rotation)
+	{
+		Vector3 objpos = obj.transform.position;
+		float x = position.x + (objpos.x - position.x) * Mathf.Cos (rotation.z * Mathf.PI / 180) - (objpos.y - position.y) * Mathf.Sin (rotation.z * Mathf.PI / 180);
+		float y = position.y + (objpos.x - position.x) * Mathf.Sin (rotation.z * Mathf.PI / 180) + (objpos.y - position.y) * Mathf.Cos (rotation.z * Mathf.PI / 180);
+
+		obj.transform.position = new Vector3 (x, y, objpos.z);
+		//obj.transform.position = Vector3.Lerp(obj.transform.position, new Vector3(x, y, objpos.z), Time.fixedDeltaTime);
+		//obj.transform.Translate(new Vector3(x, y, objpos.z));
+		obj.transform.Rotate (rotation);
+	}
+}
