e8a58004502823236826088bc2e42b894b167bb5
duongxuanlong <xuanlong.8888@gmail.com>
2019-12-21 11:49:44 +0700

    reskin objects

---
+++ RotateWheel/Assets/Script/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera.main.orthographicSize = Constant.CAMERA_HALF_HEIGHT;
+        Constant.CAMERA_HALF_WIDTH = (Constant.CAMERA_HALF_HEIGHT * Screen.width) / Screen.height;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- RotateWheel/Assets/Script/Constant.cs
+++ RotateWheel/Assets/Script/Constant.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Constant {
-	public const string DAMAGE = "Official/red_ball";
+	// public const string DAMAGE = "Official/red_ball";
 
-	public const string HEAL = "Official/green_ball";
+	// public const string HEAL = "Official/green_ball";
+	public const string DAMAGE = "new/red_ball";
+
+	public const string HEAL = "new/green_ball";
 
 	public const string PROTECT = "Official/protection";
 
-	public const string DESTROY = "destroy";
+	public const string DESTROY = "Official/destroy";
 
-	public const string PADLEFT = "Official/pad_L";
+	public const string PADLEFT = "new/pad_L";
 
-	public const string PADRIGHT = "Official/pad_R";
+	public const string PADRIGHT = "new/pad_R";
 
 	public const float HOUR_PER_SECONDS = 3600f;
 
@@ -30,6 +33,9 @@ public class Constant {
 	public static Color RED = new Color (0.933f,0.106f,0.141f);
 
 	public static Color GREEN = new Color (0.137f,0.694f,0.302f);
+
+	public const float CAMERA_HALF_HEIGHT = 6;
+	public static float CAMERA_HALF_WIDTH = 0;
 
 	//Game tutorial phase
 	public const int TUTORIAL_PHASE_0 = 0;
