c7eff82a70f705410d92e0dd47eb0fb4919ac47e
duongxuanlong <xuanlong.8888@gmail.com>
2020-01-20 14:18:29 +0700

    add new release

--- RotateWheel/Assets/Script/Social/FBMgr.cs
+++ RotateWheel/Assets/Script/Social/FBMgr.cs
@@ -30,9 +30,30 @@ public class FBMgr : MonoBehaviour
     {
         if (FB.IsInitialized)
         {
-            FB.ShareLink(new System.Uri("https://developers.facebook.com/"),
-            "MineGrab",
-            "Your Score: " + GameController.m_Instance.GetScore());
+            // FB.ShareLink(new System.Uri("https://www.google.com/"),
+            // "MineGrab",
+            // "Your Score: " + GameController.m_Instance.GetScore());
+            // https://script.google.com/macros/s/AKfycbyIsxIcGu1Lvp0KgCag0-QQGcbA4own68W3P7zyCvBrtq7SLY4/exec?BestScore=100&Score=10
+            // FB.FeedShare(link: new System.Uri("https://www.google.com/"),
+            //             linkName: "MineGrab Score: " + GameController.m_Instance.GetScore());
+            FB.FeedShare(link: new System.Uri(@"https://script.google.com/macros/s/AKfycbyIsxIcGu1Lvp0KgCag0-QQGcbA4own68W3P7zyCvBrtq7SLY4/exec?
+                                            BestScore=" + GameController.m_Instance.GetBestScore() + "&Score=" + GameController.m_Instance.GetScore()));
         }
+    }
+
+    public void ShareScoreWithScreenShot ()
+    {
+        var texture = ScreenCapture.CaptureScreenshotAsTexture();
+
+        byte[] encodedScreenshot = texture.EncodeToPNG();
+        var form = new WWWForm();
+        form.AddField("message", "Write your Description");
+        form.AddBinaryData("image", encodedScreenshot);
+        FB.API("me/photo", HttpMethod.POST, ShareScoreWithScreenShotCallback, form);
+    }
+
+    void ShareScoreWithScreenShotCallback (IResult result)
+    {
+
     }
 }
